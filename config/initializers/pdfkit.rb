5f1d6999b5d30d64d7c09d5d120093ac0b3d1430
Angie Gove <ang.gove@gmail.com>
2013-11-20 11:48:56 +1100

    pdfkit config

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -3,12 +3,12 @@ PDFKit.configure do |config|
   config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
   config.default_options = {
     :page_size => 'Legal',
-    :print_media_type => true,
-    :margin => {:top                => SIZE,                     # default 10 (mm)
-     :bottom             => SIZE,
-     :left               => SIZE,
-     :right              => SIZE},
-    :center             => 'TEXT',
+    :print_media_type => true
+    # :margin => {:top                => SIZE,                     # default 10 (mm)
+    #  :bottom             => SIZE,
+    #  :left               => SIZE,
+    #  :right              => SIZE}
+    # :center             => 'TEXT'
   }
   # Use only if your external hostname is unavailable on the server.
   config.root_url = "http://localhost"
