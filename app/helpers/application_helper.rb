caff2173d7bc16ca6c5b0146e046bf7f2d5e9ad2
Angie Gove <ang.gove@gmail.com>
2013-11-21 11:21:25 +1100

    homepage view and nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
       if false # @user_auth.is_admin
         links += "<li>#{link_to('Show Users', users_path)}</li>"
       end
-      links += "<li>#{link_to("#{@current_user.name}", user_path(@current_user))}</li>"
-      links += "<li>#{link_to('Logout '+ @current_user.name, login_path, :method => 'delete',
+      links += "<li>#{link_to("Welcome #{@current_user.name}", user_path(@current_user))}</li>"
+      links += "<li>#{link_to('Logout ', login_path, :method => 'delete',
                                 :confirm => 'Are you sure>') }</li>"
     else
       links += "<li>#{link_to('User Login', login_path) }</li>"
